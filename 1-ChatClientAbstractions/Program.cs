b3b62683719655a346e3fb3420443920a07771b6
Jose Ojeda <joche.ojeda@bitframeworks.com>
2025-02-11 17:01:39 +0300

    Structure

---
+++ 1-ChatClientAbstractions/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.AI;
+using OpenAI;
+
+namespace ChatClientAbstractions
+{
+    internal class Program
+    {
+        static IChatClient CurrentClient;
+        static string OpenAiModelId = "gpt-4o";
+        static string OllamaModelId = " phi3";
+        static async Task Main(string[] args)
+        {
+
+            CurrentClient = GetChatClientOpenAiImp(Environment.GetEnvironmentVariable("OpenAiTestKey"), OpenAiModelId);
+            //CurrentClient=GetChatClientGetOllamaImp("http://127.0.0.1:11434/", OllamaModelId);
+
+
+            var Prompt = "Describe what is c# in 20 words";
+            Console.WriteLine(Prompt);
+            
+            var Result=  await CurrentClient.CompleteAsync(Prompt);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(Result.Message);
+            Console.ReadKey();
+
+        }
+        private static IChatClient GetChatClientOpenAiImp(string ApiKey,string ModelId)
+        {
+            OpenAIClient openAIClient =new OpenAIClient(ApiKey);
+
+            return new OpenAIChatClient(openAIClient, ModelId)
+                .AsBuilder()
+                .UseFunctionInvocation()
+                .Build();
+        }
+        private static IChatClient GetChatClientGetOllamaImp(string endpoint, string modelId)
+        {
+
+            return new OllamaChatClient(endpoint, modelId: modelId)
+                .AsBuilder()
+                .UseFunctionInvocation()
+                .Build();
+        }
+    }
+}
---
+++ 2-ChatCompletions/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.AI;
+
+namespace ChatClientAbstractions
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var client=GetChatClient("", "");
+          
+        }
+        private static IChatClient GetChatClient(string endpoint, string modelId)
+        {
+
+            return new OllamaChatClient(endpoint, modelId: modelId)
+                .AsBuilder()
+                .UseFunctionInvocation()
+                .Build();
+        }
+    }
+}
