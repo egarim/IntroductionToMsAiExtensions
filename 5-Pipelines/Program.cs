3070466a9653db87f9160767b1b8b4ab7f17de98
Jose Ojeda <joche.ojeda@bitframeworks.com>
2025-02-12 09:07:49 +0300

    Rate Limiter

--- 5-Pipelines/Program.cs
+++ 5-Pipelines/Program.cs
@@ -7,46 +7,29 @@ namespace Pipelines
     {
         static IChatClient CurrentClient;
         static string OpenAiModelId = "gpt-4o";
-        static string OllamaModelId = " phi3";
+  
         static async Task Main(string[] args)
         {
 
             CurrentClient = GetChatClientOpenAiImp(Environment.GetEnvironmentVariable("OpenAiTestKey"), OpenAiModelId);
 
 
-            //Tools: A list of tools that can be used to enhance the chat experience
-            //MaxTokens: The maximum number of tokens to generate
-            //Temperature: This parameter (typically between 0 and 1) controls the randomness in the model's output
-            //TopP: What is the top p value
-            //FrequencyPenalty: What is the frequency penalty
-            //PresencePenalty: What is the presence penalty
-            //Stop: What is the stop value
-
-            var ChatOptions = new ChatOptions()
-            {
-
-                MaxOutputTokens = 10,
-            };
-
-
-            var Prompt = "Describe what is C#";
+            var Prompt = "Describe what is C# in 100 words";
             Console.WriteLine(Prompt);
 
-            var Result = await CurrentClient.CompleteAsync(Prompt, ChatOptions);
-
-            //image content
-
-            //ChatMessage Message = new ChatMessage(ChatRole.User, "Describe what is in the picture in 500 or less characters");
-
-            //Message.Contents.Add(new ImageContent(CurrentDetail.Image.MediaData, "image/jpg"));
-            //var Client = ChatClientHelper.GetChatClient();
-            //var Result = await Client.CompleteAsync(new List<ChatMessage>() { Message });
+            var Result = await CurrentClient.CompleteAsync(Prompt);
 
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(Result.Message);
             Console.ReadKey();
 
+
+            for (int i = 0; i < 10; i++)
+            {
+                 Result = await CurrentClient.CompleteAsync(Prompt);
+                 Console.WriteLine(Result.Message);
+            }
         }
         private static IChatClient GetChatClientOpenAiImp(string ApiKey, string ModelId)
         {
