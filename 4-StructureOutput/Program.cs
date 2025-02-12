06b23558250cf8f73db12c78da4cb82f25e8d90a
Jose Ojeda <joche.ojeda@bitframeworks.com>
2025-02-12 08:47:16 +0300

    structure output

--- 4-StructureOutput/Program.cs
+++ 4-StructureOutput/Program.cs
@@ -1,26 +1,48 @@
-﻿namespace StructureOutput
+﻿using Microsoft.Extensions.AI;
+using OpenAI;
+
+namespace StructureOutput
 {
     internal class Program
     {
+        static IChatClient CurrentClient;
+        static string OpenAiModelId = "gpt-4o";
         static async Task Main(string[] args)
         {
-            //var CurrentCatCollection = this.View.CurrentObject as XafNet9Ai.Module.BusinessObjects.CatCollection;
+            CurrentClient = GetChatClientOpenAiImp(Environment.GetEnvironmentVariable("OpenAiTestKey"), OpenAiModelId);
+            var Message = new ChatMessage(ChatRole.User, "Analyze this images to count the number of black cats, white cats, other animals and objects that are NOT animals");
 
-            //CurrentCatCollection.Processing = true;
-            //var Message = new ChatMessage(ChatRole.User, "Analyze this images to count the number of black cats, white cats, other animals and objects that are NOT animals");
+            //read the bytes of the image Cats.jpg
+            byte[] catsBytes = File.ReadAllBytes("Cats.jpg");
 
-            //foreach (BusinessObjects.CatCollectionDetail catCollectionDetail in CurrentCatCollection.CatCollectionDetails)
-            //{
-            //    Message.Contents.Add(new ImageContent(catCollectionDetail.Image.MediaData, "image/jpg"));
-            //}
-            //var Client = ChatClientHelper.GetChatClient();
+            //read the bytes of the image Puppies.jpg
+            byte[] PuppiesBytes = File.ReadAllBytes("Puppies.jpg");
 
-
+            //read the bytes of the image Robots.jpg
+            byte[] RobotsBytes = File.ReadAllBytes("Robots.jpg");
 
 
-            //List<Func<Task<ChatCompletion>>> funcs = new List<Func<Task<ChatCompletion>>>();
-            //funcs.Add(async () => { return await Client.CompleteAsync<CatCollectionDescription>(new List<ChatMessage>() { Message }); });
+            Message.Contents.Add(new ImageContent(catsBytes, "image/jpg"));
+            Message.Contents.Add(new ImageContent(PuppiesBytes, "image/jpg"));
+            Message.Contents.Add(new ImageContent(RobotsBytes, "image/jpg"));
 
+            ChatCompletion<CatCollectionDescription> Answer = await CurrentClient.CompleteAsync<CatCollectionDescription>(new List<ChatMessage>() { Message });
+
+            Console.WriteLine($"Number of black cats: {Answer.Result.NumberOfBlackCats}");
+            Console.WriteLine($"Number of white cats: {Answer.Result.NumberOfWhiteCats}");
+            Console.WriteLine($"Number of other animals: {Answer.Result.NumberOfOtherAnimals}");
+            Console.WriteLine($"Number of other objects that are NOT animals: {Answer.Result.NumberOfNotAnimals}");
+
+            Console.ReadKey();
+        }
+
+        private static IChatClient GetChatClientOpenAiImp(string ApiKey, string ModelId)
+        {
+            OpenAIClient openAIClient = new OpenAIClient(ApiKey);
+
+            return new OpenAIChatClient(openAIClient, ModelId)
+                .AsBuilder()
+                .Build();
         }
     }
 }
